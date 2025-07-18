7aa1553883e036b0ba84a43d0cb82019c21dd8be
Abdalrzag Eisa <bmjfdrh@gmail.com>
2025-07-18 09:36:18 +0300

    chore: build firefox extension script

--- build-firefox-extension.sh
+++ build-firefox-extension.sh
@@ -1,5 +1,29 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-# TODO: pnpm install
-# TODO: pnpm build
-# TODO: ZIP dist
+set -eo pipefail
+
+XPI_TARGET="${PWD}"/chianti-firefox.xpi
+NODE_PACKAGE_MANAGER="pnpm"
+
+if ! command -v "${NODE_PACKAGE_MANAGER}" >/dev/null 2>&1; then
+    echo "${NODE_PACKAGE_MANAGER} is not installed"
+    echo "Using npm!"
+    NODE_PACKAGE_MANAGER="npm"
+fi
+
+if ! command -v "7z" >/dev/null 2>&1; then
+    echo "7z is not installed"
+    exit 1
+fi
+
+cd firefox-extension
+
+"${NODE_PACKAGE_MANAGER}" install
+"${NODE_PACKAGE_MANAGER}" run build
+
+if [[ -f "${XPI_TARGET}" ]]; then
+    rm -v "${XPI_TARGET}"
+fi
+
+cd dist
+7z a "${XPI_TARGET}" ./*
