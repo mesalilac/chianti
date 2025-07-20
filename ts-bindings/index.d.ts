6668fdada99d2dc5bf03501a0d902e3adb606eec
Abdalrzag Eisa <bmjfdrh@gmail.com>
2025-07-20 05:49:15 +0300

    chore: export bindings

--- ts-bindings/index.d.ts
+++ ts-bindings/index.d.ts
@@ -1,3 +1,5 @@
 // Auto-generated file using export_bindings.sh
 
-export * from './bindings/CreateWatchHistoryRequest.ts';
+export type * from './bindings/CreateWatchHistoryVideo.ts';
+export type * from './bindings/CreateWatchHistoryChannel.ts';
+export type * from './bindings/CreateWatchHistoryRequest.ts';
