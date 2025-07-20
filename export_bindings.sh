1040743adae05fccd340493ba1e9583bb3fa1993
Abdalrzag Eisa <bmjfdrh@gmail.com>
2025-07-20 05:39:22 +0300

    chore: add logging

--- export_bindings.sh
+++ export_bindings.sh
@@ -3,10 +3,16 @@
 set -e
 
 if [ -d "./ts-bindings/bindings" ]; then
+    echo "Removing old bindings"
+    echo "--------------------------"
     rm -rv ./ts-bindings/bindings
+    echo "--------------------------"
 fi
 
+echo "Exporting bindings"
+echo "--------------------------"
 cargo test export_bindings
+echo "--------------------------"
 
 cd ./ts-bindings/
 
@@ -15,5 +21,5 @@ TYPES_EXPORT_FILE="index.d.ts"
 printf "// Auto-generated file using export_bindings.sh\n\n" > "${TYPES_EXPORT_FILE}"
 
 for file in $(find ./bindings -name "*.ts"); do
-    printf "export * from \'%s\';\n" "${file}" >> "${TYPES_EXPORT_FILE}"
+    printf "export type * from \'%s\';\n" "${file}" >> "${TYPES_EXPORT_FILE}"
 done
