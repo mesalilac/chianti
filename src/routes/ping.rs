8e0b20bf3678ae2d5f1711732d1bacd875a97056
Abdalrzag Eisa <bmjfdrh@gmail.com>
2025-07-28 05:16:37 +0300

    refactor(server): Return `pong`

--- src/routes/ping.rs
+++ src/routes/ping.rs
@@ -2,5 +2,5 @@ use axum::http::StatusCode;
 
 /// check if the server is online
 pub async fn ping() -> (StatusCode, String) {
-    (StatusCode::OK, "Server is online".to_string())
+    (StatusCode::OK, "pong".to_string())
 }
