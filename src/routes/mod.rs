5c1aed532cc49eb5706ece17bd53ea573a22f896
Abdalrzag Eisa <bmjfdrh@gmail.com>
2025-07-29 19:39:42 +0300

    feat(server): `overview` route

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,5 +1,6 @@
 mod channel_avater;
 mod handle_404;
+mod overview;
 mod ping;
 mod root;
 mod video_thumbnail;
@@ -13,10 +14,15 @@ use axum::{
 
 pub use channel_avater::get_channel_avater;
 pub use handle_404::handle_404;
+pub use overview::get_overview;
 pub use ping::ping;
 pub use root::root;
 pub use video_thumbnail::get_video_thumbnail;
 pub use watch_history::create_watch_history;
+
+pub fn stats_routes() -> Router<AppState> {
+    Router::new().route("/overview", get(get_overview))
+}
 
 pub fn api_routes() -> Router<AppState> {
     Router::new()
@@ -24,4 +30,5 @@ pub fn api_routes() -> Router<AppState> {
         .route("/thumbnail/{video_id}", get(get_video_thumbnail))
         .route("/ping", get(ping))
         .route("/watch_history", post(create_watch_history))
+        .nest("/stats", stats_routes())
 }
---
+++ src/routes/overview.rs
@@ -0,0 +1,78 @@
+use crate::database::models::{Channel, Tag, Video};
+use crate::state::AppState;
+use crate::utils::internal_error;
+use crate::{database::models::WatchHistory, schema};
+use axum::{Json, extract::State, http::StatusCode};
+use diesel::prelude::*;
+use serde::{Deserialize, Serialize};
+use ts_rs::TS;
+
+#[derive(Serialize, Deserialize, TS)]
+#[ts(export)]
+pub struct OverviewResponse {
+    #[ts(type = "number")]
+    pub total_watch_time_seconds: i64,
+    #[ts(type = "number")]
+    pub total_videos_watched: i64,
+    #[ts(type = "number")]
+    pub total_unique_videos_watched: i64,
+    #[ts(type = "number")]
+    pub total_channels: i64,
+    #[ts(type = "number")]
+    pub total_tags: i64,
+    #[ts(type = "number")]
+    pub average_watch_time_per_session_seconds: i64,
+}
+
+pub async fn get_overview(
+    State(state): State<AppState>,
+) -> Result<(StatusCode, Json<OverviewResponse>), (StatusCode, String)> {
+    use schema::channels::dsl as channels_dsl;
+    use schema::tags::dsl as tags_dsl;
+    use schema::videos::dsl as videos_dsl;
+    use schema::watch_history::dsl as watch_history_dsl;
+
+    let mut conn = state.pool.get().map_err(internal_error)?;
+
+    let watch_history_list = watch_history_dsl::watch_history
+        .load::<WatchHistory>(&mut conn)
+        .map_err(internal_error)?;
+
+    let videos_list = videos_dsl::videos
+        .load::<Video>(&mut conn)
+        .map_err(internal_error)?;
+
+    let channels_list = channels_dsl::channels
+        .load::<Channel>(&mut conn)
+        .map_err(internal_error)?;
+
+    let tags_list = tags_dsl::tags
+        .load::<Tag>(&mut conn)
+        .map_err(internal_error)?;
+
+    let total_watch_time_seconds = watch_history_list
+        .iter()
+        .map(|s| s.watch_duration_seconds)
+        .sum::<i64>();
+    let total_videos_watched = videos_list.iter().map(|s| s.watch_counter).sum::<i64>();
+    let total_unique_videos_watched = videos_list.len() as i64;
+    let average_watch_time_per_session_seconds = watch_history_list
+        .iter()
+        .map(|s| s.watch_duration_seconds)
+        .sum::<i64>()
+        / watch_history_list.len() as i64;
+    let total_channels = channels_list.len() as i64;
+    let total_tags = tags_list.len() as i64;
+
+    Ok((
+        StatusCode::OK,
+        Json(OverviewResponse {
+            total_watch_time_seconds,
+            total_videos_watched,
+            total_channels,
+            total_tags,
+            total_unique_videos_watched,
+            average_watch_time_per_session_seconds,
+        }),
+    ))
+}
