6dd49e66ecbbfb3d39cbb4e069f0099fd2981855
Abdalrzag Eisa <bmjfdrh@gmail.com>
2025-07-31 04:40:52 +0300

    feat(server): use `OpenApiRouter`

--- src/routes/api/mod.rs
+++ src/routes/api/mod.rs
@@ -5,22 +5,15 @@ mod video_thumbnail;
 mod watch_history;
 
 use crate::state::AppState;
-use axum::{
-    Router,
-    routing::{get, post},
-};
+use utoipa_axum::{router::OpenApiRouter, routes};
 
-use channel_avater::get_channel_avater;
-use ping::ping;
 use stats::stats_routes;
-use video_thumbnail::get_video_thumbnail;
-use watch_history::create_watch_history;
 
-pub fn api_routes() -> Router<AppState> {
-    Router::new()
-        .route("/avater/{channel_id}", get(get_channel_avater))
-        .route("/thumbnail/{video_id}", get(get_video_thumbnail))
-        .route("/ping", get(ping))
-        .route("/watch_history", post(create_watch_history))
+pub fn api_routes() -> OpenApiRouter<AppState> {
+    OpenApiRouter::new()
+        .routes(routes!(ping::ping))
+        .routes(routes!(channel_avater::get_channel_avater))
+        .routes(routes!(video_thumbnail::get_video_thumbnail))
+        .routes(routes!(watch_history::create_watch_history))
         .nest("/stats", stats_routes())
 }
