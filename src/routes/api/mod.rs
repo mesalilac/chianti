e0d4202805600fb8e20736ff38cece944c1e7c40
Abdalrzag Eisa <bmjfdrh@gmail.com>
2025-08-02 01:56:34 +0300

    refactor(server): change module name to match route

--- src/routes/api/mod.rs
+++ src/routes/api/mod.rs
@@ -1,13 +1,13 @@
 mod channel_avater;
 mod ping;
-mod stats;
+mod statistics;
 mod video_thumbnail;
 mod watch_history;
 
 use crate::state::AppState;
 use utoipa_axum::{router::OpenApiRouter, routes};
 
-use stats::stats_routes;
+use statistics::statistics_routes;
 
 pub fn api_routes() -> OpenApiRouter<AppState> {
     OpenApiRouter::new()
@@ -15,5 +15,5 @@ pub fn api_routes() -> OpenApiRouter<AppState> {
         .routes(routes!(channel_avater::get_channel_avater))
         .routes(routes!(video_thumbnail::get_video_thumbnail))
         .routes(routes!(watch_history::create_watch_history))
-        .nest("/statistics", stats_routes())
+        .nest("/statistics", statistics_routes())
 }
