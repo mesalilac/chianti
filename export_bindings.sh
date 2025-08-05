8457e03796295711deb0e0b17e06a7efbff1408d
Abdalrzag Eisa <bmjfdrh@gmail.com>
2025-08-05 11:07:48 +0300

    feat: use `ts-rs` cli tool

--- export_bindings.sh
+++ export_bindings.sh
@@ -2,24 +2,6 @@
 
 set -e
 
-if [ -d "./ts-bindings/bindings" ]; then
-    echo "Removing old bindings"
-    echo "--------------------------"
-    rm -rv ./ts-bindings/bindings
-    echo "--------------------------"
-fi
+# cargo install --git https://github.com/Aleph-Alpha/ts-rs --branch feat/cli ts-rs-cli
 
-echo "Exporting bindings"
-echo "--------------------------"
-cargo test export_bindings
-echo "--------------------------"
-
-cd ./ts-bindings/
-
-TYPES_EXPORT_FILE="index.d.ts"
-
-printf "// Auto-generated file using export_bindings.sh\n\n" > "${TYPES_EXPORT_FILE}"
-
-for file in $(find ./bindings -name "*.ts"); do
-    printf "export * from \'%s\';\n" "${file}" >> "${TYPES_EXPORT_FILE}"
-done
+ts-rs export --index
--- ts-bindings/index.d.ts
+++
@@ -1,5 +0,0 @@
-// Auto-generated file using export_bindings.sh
-
-export * from './bindings/CreateWatchHistoryVideo.ts';
-export * from './bindings/CreateWatchHistoryChannel.ts';
-export * from './bindings/CreateWatchHistoryRequest.ts';
