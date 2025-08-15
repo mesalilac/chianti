2f3a9cc4110460dc06b3bf5557e03767b23b3516
Abdalrzag Eisa <bmjfdrh@gmail.com>
2025-08-15 13:48:47 +0300

    chore(server): remove `--format`

--- export_bindings.sh
+++ export_bindings.sh
@@ -9,4 +9,4 @@ EXPORT_TARGET="./ts-bindings"
 [ -d "${EXPORT_TARGET}" ] && echo "Removing old ${EXPORT_TARGET}" && rm -r "${EXPORT_TARGET}"
 
 echo "Exporting bindings to ${EXPORT_TARGET}"
-ts-rs export --index --format --output-directory "${EXPORT_TARGET}"
+ts-rs export --index --output-directory "${EXPORT_TARGET}"
