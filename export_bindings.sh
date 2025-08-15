c4a4b1cd3214b3df81375869d630f8fd70896184
Abdalrzag Eisa <bmjfdrh@gmail.com>
2025-08-15 13:46:37 +0300

    chore(server): improve script

--- export_bindings.sh
+++ export_bindings.sh
@@ -4,6 +4,9 @@ set -e
 
 # cargo install --git https://github.com/Aleph-Alpha/ts-rs --branch feat/cli ts-rs-cli
 
-rm -rv ./ts-bindings
+EXPORT_TARGET="./ts-bindings"
 
-ts-rs export --index
+[ -d "${EXPORT_TARGET}" ] && echo "Removing old ${EXPORT_TARGET}" && rm -r "${EXPORT_TARGET}"
+
+echo "Exporting bindings to ${EXPORT_TARGET}"
+ts-rs export --index --format --output-directory "${EXPORT_TARGET}"
